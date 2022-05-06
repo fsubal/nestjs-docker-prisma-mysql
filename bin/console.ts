2a71c86de3638cfe07ec826f0036ff028f97d9ac
fsubal <fsubal1102@gmail.com>
2022-05-07 01:02:27 +0900

    strict: true

--- bin/console.ts
+++ bin/console.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  // @ts-expect-error 型 'typeof globalThis' にはインデックス シグネチャがないため、要素は暗黙的に 'any' 型になります。ts(7017)
   globalThis.app = app;
+
+  // @ts-expect-error 型 'typeof globalThis' にはインデックス シグネチャがないため、要素は暗黙的に 'any' 型になります。ts(7017)
   globalThis.prisma = app.get(PrismaService);
 }
 
