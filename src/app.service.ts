5bab49660d699e07d87e5931035c393144a759ff
fsubal <fsubal1102@gmail.com>
2022-05-06 15:11:34 +0900

    add Item model and respond json

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Is it such a Hello World';
+  constructor(private prisma: PrismaService) {}
+
+  async getHello() {
+    const all = await this.prisma.item.findMany();
+    return all;
   }
 }
