eaf877ea044d58ce25261526486698dd58370b35
fsubal <fsubal1102@gmail.com>
2022-05-08 15:48:37 +0900

    wip: introduce minio

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,21 @@ import { RootController } from './controllers/root.controller';
 import { ItemController } from './controllers/api/v1/item.controller';
 import { PrismaService } from './services/prisma/prisma.service';
 import { ItemService } from './services/item/item.service';
+import { UploaderService } from './services/uploader/uploader.service';
+import { ConfigModule } from '@nestjs/config';
+import { ENV } from './env';
 
 @Module({
-  imports: [I18nModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate(config) {
+        ENV.parse(config);
+        return config;
+      },
+    }),
+    I18nModule,
+  ],
   controllers: [RootController, ItemController],
-  providers: [PrismaService, ItemService],
+  providers: [PrismaService, ItemService, UploaderService],
 })
 export class AppModule {}
